79507bbcbcf092dbcb31f5eebfb8cd58d3fa1c7e
joanvasquez21 <joanvrivas.21@gmail.com>
2024-11-22 17:29:43 +0100

    feat:add db

---
+++ springsecurity-joan/src/main/java/com/joan/SpringsecurityJoanApplication.java
@@ -0,0 +1,13 @@
+package com.joan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurityJoanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecurityJoanApplication.class, args);
+	}
+
+}
---
+++ springsecurity-joan/src/test/java/com/joan/SpringsecurityJoanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityJoanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
