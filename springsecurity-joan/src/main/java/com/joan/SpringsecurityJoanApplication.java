4b67c14a196257a3122e7c2210b072f0051402d1
joanvasquez21 <joanvrivas.21@gmail.com>
2024-11-28 23:26:46 +0100

    feat: custom error messages added

--- springsecurity-joan/src/main/java/com/joan/SpringsecurityJoanApplication.java
+++ springsecurity-joan/src/main/java/com/joan/SpringsecurityJoanApplication.java
@@ -2,8 +2,10 @@ package com.joan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:messages.properties")
 public class SpringsecurityJoanApplication {
 
 	public static void main(String[] args) {
